42c15adc42b1719d2d6f3e8e945fc3247412440a
Moonclood <62996154+Gvatemala228@users.noreply.github.com>
2021-11-27 20:48:47 +0300

    Навигационная панель

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,8 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title_tag')</title>
+<link href="{{url('css/app.css')}}" rel="stylesheet" />
 </head>
 <body>
+<div class="wrapper">
 <header>
-
+<div class="container">
+    <div class="nav">
+        <nav>
+            <a><h1>Блог</h1></a>
+            <ul class="menu">
+                <li class="nav__btn"><a href={{route('home')}}>Главная</a></li>
+                <li class="nav__btn"><a>Посты</a></li>
+                <li class="nav__btn"><a>Категории</a></li>
+                <li class="nav__btn"><a>Профиль</a></li>
+            </ul>
+        </nav>
+    </div>
+</div>
 </header>
