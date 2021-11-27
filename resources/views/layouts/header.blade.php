9e98e38138ab67b30857b8769c9e47ef8be8978d
Moonclood <62996154+Gvatemala228@users.noreply.github.com>
2021-11-27 21:22:16 +0300

    Ссылка на страницу с постами

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,7 +16,7 @@
             <a><h1>Блог</h1></a>
             <ul class="menu">
                 <li class="nav__btn"><a href={{route('home')}}>Главная</a></li>
-                <li class="nav__btn"><a>Посты</a></li>
+            <li class="nav__btn"><a href={{route('posts.index')}}>Посты</a></li>
                 <li class="nav__btn"><a>Категории</a></li>
                 <li class="nav__btn"><a>Профиль</a></li>
             </ul>
