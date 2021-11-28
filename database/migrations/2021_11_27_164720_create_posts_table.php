b404996e36de434cf31cc667a257c98d7b09122d
Moonclood <62996154+Gvatemala228@users.noreply.github.com>
2021-11-28 12:45:56 +0300

    Добавил author_id

--- database/migrations/2021_11_27_164720_create_posts_table.php
+++ database/migrations/2021_11_27_164720_create_posts_table.php
@@ -15,6 +15,7 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('author_id');
             $table->string('title');
             $table->text('content');
             $table->timestamps();
