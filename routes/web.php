57787c6a86df95b86e91f6c76988b79a3f074549
Moonclood <62996154+Gvatemala228@users.noreply.github.com>
2021-11-28 13:00:39 +0300

    Route /posts/create

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::get('/', 'HomeController')->name('home');
 
 Route::group(['namespace' => 'Posts'], function () {
     Route::get('/posts', 'IndexController')->name('posts.index');
+    Route::get('/posts/create', 'CreateController')->name('posts.create');
 });
 
 Auth::routes();
