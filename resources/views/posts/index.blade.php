e4a243a67bc2150f3d3663695f3c4382ebecedf0
Moonclood <62996154+Gvatemala228@users.noreply.github.com>
2021-12-04 18:08:41 +0300

    Стилизация страницы со всеми постами

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,13 +2,19 @@
 @section('title_tag')Все посты@endsection
 @section('content_section')
 <div class="container">
+    <div class="card-group">
     @foreach($posts as $post)
-    <div>
-    <a href={{route('posts.show',$post->id)}}>{{$post->title}}</a>
+    <div class="col">
+    <div class="card">
+    <a href={{route('posts.show',$post->id)}}><img class="card-img-top" src="{{asset('storage/'.$post->image)}}" alt=""></a>
+    <div class="card-body">
+    <a href={{route('posts.show',$post->id)}}><h5 class="card-title" href={{route('posts.show',$post->id)}}>{{$post->title}}</h5></a>
+    <p>Автор: <a href="{{route('profile.show',$post->author->id)}}">{{$post->author->name}}</a></p>
+    <p class="card-text"><small class="text-muted">{{$post->created_at->format('d/m/Y')}}</small></p>
     </div>
-    {{-- </div> --}}
-    {{-- {{$post->author}} --}}
-    {{-- </div> --}}
+    </div>
+</div>
     @endforeach
+</div>
 </div>
 @endsection
