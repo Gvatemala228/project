55435b20e86e695e3bb1451c28305e1d1c025264
Moonclood <62996154+Gvatemala228@users.noreply.github.com>
2021-12-04 19:00:07 +0300

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title_tag')</title>
-<link href="{{url('css/app.css')}}" rel="stylesheet" />
+    <link rel="stylesheet" href="{{url('css/app.css')}}" />
+    <link rel="stylesheet" href="{{url('css/style.css')}}">
 </head>
 <body>
 <div class="wrapper">
