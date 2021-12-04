602ed96fd22dbb8b2ae2a58b011276b4910fe61b
Moonclood <62996154+Gvatemala228@users.noreply.github.com>
2021-12-04 22:31:05 +0300

    Update show.blade.php

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -2,10 +2,10 @@
 @section('title_tag')Профиль пользователя {{$user->id}}@endsection
 @section('content_section')
 <div class="container">
-    <h1>Профиль пользователя {{$user->id}}</h1>
-    {{-- <div>Логин: {{$user->login}}</div> --}}
+    <h1>Профиль пользователя {{$user->login}}</h1>
+    <div>Логин: {{$user->login}}</div>
     <div>Имя: {{$user->name}}</div>
-    {{-- <div>Фамилия: {{$user->surname}}</div> --}}
+    <div>Фамилия: {{$user->surname}}</div>
     <div>Email: {{$user->email}}</div>
     <div><a href="{{route('profile.userPosts', $user->id)}}">Посты</a></div>
     <br>
