3a7a1dcadadade97661a4402fb4494f6c708da83
Moonclood <62996154+Gvatemala228@users.noreply.github.com>
2021-12-05 00:19:01 +0300

    Update UpdateController.php

--- app/Http/Controllers/Profile/UpdateController.php
+++ app/Http/Controllers/Profile/UpdateController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Profile;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\UpdateProfileRequest;
 
