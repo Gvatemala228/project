14fa80a8504eea2beac3f1c467e2bed9ec21342c
Moonclood <62996154+Gvatemala228@users.noreply.github.com>
2021-12-04 12:06:01 +0300

    Update userPosts.blade.php

--- resources/views/profile/userPosts.blade.php
+++ resources/views/profile/userPosts.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.main')
-@section('title_tag')Посты пользователя@endsection
+@section('title_tag')Посты пользователя {{$id}}@endsection
 @section('content_section')
 <div class="container">
-    <h1>Посты пользователя</h1>
+    <h1>Посты пользователя {{$id}}</h1>
     @foreach($posts as $post)
     <div><a href="{{route('posts.show',$post->id)}}">{{$post->title}}</a></div>
     <br>
