4b351724487742c259c8c4e647c9a49e4036ed18
Moonclood <62996154+Gvatemala228@users.noreply.github.com>
2021-12-04 19:00:05 +0300

    Update index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,9 +4,9 @@
 <div class="container">
     <div class="card-group">
     @foreach($posts as $post)
-    <div class="col">
-    <div class="card">
-    <a href={{route('posts.show',$post->id)}}><img class="card-img-top" src="{{asset('storage/'.$post->image)}}" alt=""></a>
+    <div class="col-sm-4 mb-3">
+    <div class="card h-100">
+    <a href={{route('posts.show',$post->id)}}><img class="card-img-top preview-image" src="{{asset('storage/'.$post->image)}}" alt=""></a>
     <div class="card-body">
     <a href={{route('posts.show',$post->id)}}><h5 class="card-title" href={{route('posts.show',$post->id)}}>{{$post->title}}</h5></a>
     <p>Автор: <a href="{{route('profile.show',$post->author->id)}}">{{$post->author->name}}</a></p>
