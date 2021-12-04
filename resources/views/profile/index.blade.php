ca435b23a516e0fd2c0273b07fe4a2ffcf7ae9f1
Moonclood <62996154+Gvatemala228@users.noreply.github.com>
2021-12-04 23:14:36 +0300

    Update index.blade.php

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -2,10 +2,11 @@
 @section('title_tag')Ваш профиль@endsection
 @section('content_section')
 <div class="container">
+    <div class="px-3">
     <h1>Ваш профиль</h1>
-    {{-- <div>Логин: {{$user->login}}</div> --}}
+    <div>Логин: {{$user->login}}</div>
     <div>Имя: {{$user->name}}</div>
-    {{-- <div>Фамилия: {{$user->surname}}</div> --}}
+    <div>Фамилия: {{$user->surname}}</div>
     <div>Email: {{$user->email}}</div>
     <div><a href="{{route('profile.posts')}}">Ваши посты</a> | <a href="{{route('profile.edit')}}">Редактировать</a></div>
     <br>
@@ -13,5 +14,6 @@
     @csrf
         <input type="submit" class="btn btn-danger" value="Выйти"/>
     </form>
+</div>
 </div>
 @endsection
