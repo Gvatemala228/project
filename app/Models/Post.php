5982713f6e40b880cbd9687571aef5a2dd15a89b
Moonclood <62996154+Gvatemala228@users.noreply.github.com>
2021-12-14 23:02:19 +0300

    Вывод комментариев

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -22,4 +22,9 @@ class Post extends Model
     {
         return $this->belongsTo(Category::class, 'category_id', 'id');
     }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class, 'post_id', 'id');
+    }
 }
