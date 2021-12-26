515505ffa02028bda0d2a76d326b8ea2419a7f4f
Moonclood <62996154+Gvatemala228@users.noreply.github.com>
2021-12-26 14:26:43 +0300

    Проверка на автора комментария

--- app/Http/Controllers/Posts/Comments/DestroyController.php
+++ app/Http/Controllers/Posts/Comments/DestroyController.php
@@ -2,15 +2,19 @@
 
 namespace App\Http\Controllers\Posts\Comments;
 
-use Illuminate\Http\Request;
 use App\Models\Comment;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class DestroyController extends Controller
 {
     public function __invoke($id)
     {
         $comment = Comment::find($id);
+        if (Auth::user()->id != $comment->author_id) {
+            return abort(403);
+        }
         $comment->delete();
         return response()->json(['response' => 'Успешное удаление комментария']);
     }
